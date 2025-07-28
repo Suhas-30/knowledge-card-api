7a0e6887fab553543138f59713c6535dbb8cbfa1
Suhas-30 <3suhashs@gmail.com>
2025-07-28 06:42:29 +0530

    Day 5: View, search, and delete cards

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import * as Cheerio from "cheerio";
 import { generateSummary } from "../services/openaiServices.js";
 import Card from "../models/card.js";
+import { where } from "sequelize";
 
 export const generatedCard = async (req, res) => {
   const { sourceType, content } = req.body;
@@ -54,5 +55,46 @@ export const generatedCard = async (req, res) => {
     return res.status(201).json({ message: "Card saved", card });
   } catch (error) {
     return res.status(500).json({ error: "Failed to save card" });
+  }
+};
+
+export const getCards = async(req, res) =>{
+  try{
+    const card = await Card.findAll({where: {userId: req.user.id}});
+    res.status(200).json({cards});
+  }catch(err){
+    res.status(500).json({error: "Failed to featch cards"});
+
+  }
+}
+
+export const searchCards = async(req, res)=>{
+  const {tags} = tags.query;
+  try{
+    const cards = await Card.findAll({
+      where:{
+        userId:req.user.id,
+        tags:{
+          [Op.contains] : [tags.toLowerCase()],
+        }
+      }
+    })
+    res.status(200).json({cards})
+  }catch(error){
+    res.status(500).json({error:"Search failed"})
+  }
+}
+
+
+export const deleteCard = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const card = await Card.findOne({ where: { id, userId: req.user.id } });
+    if (!card) return res.status(404).json({ error: "Card not found" });
+
+    await card.destroy();
+    res.status(200).json({ message: "Card deleted" });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to delete card" });
   }
 };
--- routes/cardRoutes.js
+++ routes/cardRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { generatedCard } from "../controllers/cardController.js";
+import { deleteCard, generatedCard, searchCards } from "../controllers/cardController.js";
 import verifyToken from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/generate", verifyToken, generatedCard);
+router.get("/", verifyToken, getCards);
+router.get("/search", verifyToken, searchCards);
+router.delete("/:id", verifyToken, deleteCard);
 
 export default router;
